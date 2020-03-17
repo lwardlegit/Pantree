58a3cb5adeca5953c7e6f7b40cdd1002302d92de
lwardlegit <42820152+lwardlegit@users.noreply.github.com>
2020-03-17 14:41:46 -0400

    working version

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('client_secret.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), listMajors);
+  authorize(JSON.parse(content), listPantry);
 });
 
 /**
@@ -111,10 +111,10 @@ function getNewToken(oAuth2Client, callback) {
  * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-function listMajors(auth) {
+function listPantry(auth) {
   const sheets = google.sheets({version: 'v4', auth});
   sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    spreadsheetId: '1VTiTgPvMvLlOVBhBfoMGrshfMMJJMMzoA6GesheWMPg',
     range: 'Class Data!A2:E',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
@@ -123,7 +123,7 @@ function listMajors(auth) {
       console.log('Name, Major:');
       // Print columns A and E, which correspond to indices 0 and 4.
       rows.map((row) => {
-        console.log(`${row[0]}, ${row[4]}`);
+        console.log(`${row[0]}, ${row[1]}, ${row[2]}`);
       });
     } else {
       console.log('No data found.');
